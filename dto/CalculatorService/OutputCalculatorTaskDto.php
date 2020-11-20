2cc81680860fa5449d119aa0167b4196a345da5d
dmitriy <dnezhkin@kuber.ru>
2020-11-20 10:54:14 +0300

    201120-1054: update OutputCalculatorTaskDto.php

--- dto/CalculatorService/OutputCalculatorTaskDto.php
+++ dto/CalculatorService/OutputCalculatorTaskDto.php
@@ -1,11 +1,13 @@
 <?php
 namespace Kubersoftware\Dto\CalculatorService;
 
+use DateTime;
 
 class OutputCalculatorTaskDto
 {
     private string $taskGuid;
     private float $orderResult;
+    private DateTime $updateAt;
 
     /**
      * @return string
@@ -40,6 +42,24 @@ class OutputCalculatorTaskDto
     public function setOrderResult(float $orderResult): OutputCalculatorTaskDto
     {
         $this->orderResult = $orderResult;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdateAt(): DateTime
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * @param DateTime $updateAt
+     * @return OutputCalculatorTaskDto
+     */
+    public function setUpdateAt(DateTime $updateAt): OutputCalculatorTaskDto
+    {
+        $this->updateAt = $updateAt;
         return $this;
     }
 }
