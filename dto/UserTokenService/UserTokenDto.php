1dad7f3102c6e3e25cd7f81a8ec3a8f0f0205e0b
dmitriy <dnezhkin@kuber.ru>
2020-11-23 09:29:09 +0300

    221120-0928: update UserTokenDto.php

--- dto/UserTokenService/UserTokenDto.php
+++ dto/UserTokenService/UserTokenDto.php
@@ -2,82 +2,15 @@
 
 namespace Kubersoftware\Dto\UserTokenService;
 
+use DateTime;
+
 class UserTokenDto
 {
-    private string $userUuid;
-    private string $userToken;
-    private string $createdAt;
-    private string $livedAt;
+    private string $userEmail;
+    private string $userCsrfToken;
+    private array $userRoles;
+    private string $sessionID;
+    private DateTime $createdAt;
+    private DateTime $livedAt;
 
-    /**
-     * @return string
-     */
-    public function getUserUuid(): string
-    {
-        return $this->userUuid;
-    }
-
-    /**
-     * @param string $userUuid
-     * @return UserTokenDto
-     */
-    public function setUserUuid(string $userUuid): UserTokenDto
-    {
-        $this->userUuid = $userUuid;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUserToken(): string
-    {
-        return $this->userToken;
-    }
-
-    /**
-     * @param string $userToken
-     * @return UserTokenDto
-     */
-    public function setUserToken(string $userToken): UserTokenDto
-    {
-        $this->userToken = $userToken;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCreatedAt(): string
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param string $createdAt
-     * @return UserTokenDto
-     */
-    public function setCreatedAt(string $createdAt): UserTokenDto
-    {
-        $this->createdAt = $createdAt;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLivedAt(): string
-    {
-        return $this->livedAt;
-    }
-
-    /**
-     * @param string $livedAt
-     * @return UserTokenDto
-     */
-    public function setLivedAt(string $livedAt): UserTokenDto
-    {
-        $this->livedAt = $livedAt;
-        return $this;
-    }
 }
--- enum/MongodbCollectionsEnum.php
+++ enum/MongodbCollectionsEnum.php
@@ -7,5 +7,6 @@ namespace Kubersoftware\Enum;
 class MongodbCollectionsEnum
 {
     public const USER_TOKENS_LIFETIME_IN_HOURS = 8760;
+    public const USER_TOKENS_DATETIME_FORMAT = 'm-d-Y H:i:s';
     public const USER_TOKENS_COLLECTION = "user_tokens";
 }
