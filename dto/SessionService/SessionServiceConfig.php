427635ca09b118f13d0fef9512f8d9cd80b56154
dmitriy <dnezhkin@kuber.ru>
2020-11-23 14:29:36 +0300

    221120-1428: add SessionServiceConfig.php, rename MongodbCollectionsEnum.php to MongoCollectionsEnum.php, update composer.json

--- dto/SessionService/SessionServiceConfig.php
+++ dto/SessionService/SessionServiceConfig.php
@@ -4,9 +4,8 @@
 namespace Kubersoftware\Dto\SessionService;
 
 
-class SessionServiceEnum
+class SessionServiceConfig
 {
     public const USER_SESSIONS_LIFETIME_IN_HOURS = 8760;
     public const USER_SESSIONS_DATETIME_FORMAT = 'm-d-Y H:i:s';
-    public const USER_SESSIONS_MONGO_COLLECTION = "user_sessions";
 }
---
+++ enum/MongoCollectionsEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Kubersoftware\Enum;
+
+
+class MongoCollectionsEnum
+{
+    public const USER_SESSIONS_COLLECTION = "user_sessions";
+}
