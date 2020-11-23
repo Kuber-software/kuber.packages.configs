0c99d56fcb0a9f9e39ba90c27d8046b11f06fefd
dmitriy <dnezhkin@kuber.ru>
2020-11-23 12:58:01 +0300

    221120-1257: update MongodbCollectionsEnum.php

--- enum/MongodbCollectionsEnum.php
+++ enum/MongodbCollectionsEnum.php
@@ -8,5 +8,5 @@ class MongodbCollectionsEnum
 {
     public const USER_TOKENS_LIFETIME_IN_HOURS = 8760;
     public const USER_TOKENS_DATETIME_FORMAT = 'm-d-Y H:i:s';
-    public const USER_TOKENS_COLLECTION = "user_tokens";
+    public const USER_TOKENS_COLLECTION = "user_sessions";
 }
