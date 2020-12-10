b2d7abbc8e962d0d204274c842b2d758ff24b04c
kulesh-i <Icf?8bj3dGFk>
2020-12-10 18:35:22 +0300

    101220-1833: test comit

--- microservices/MicroservicesListEnum.php
+++ microservices/MicroservicesListEnum.php
@@ -24,8 +24,6 @@ class MicroservicesListEnum
     public const USERS = "Users";
     public const SCORING = "Scoring";
 
-    // coometn
-
     /**
      * Имя микросервиса из констант класса @MicroservicesListEnum
      *
